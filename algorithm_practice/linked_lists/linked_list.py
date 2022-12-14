59137b31d6dfe84e94651ed300a0e0b59ace5336
nadxelle <nadxellehernandez@gmail.com>
2022-12-14 14:30:15 -0800

    Added a Single linked list implementation with a is_palindrome method

---
+++ algorithm_practice/linked_lists/linked_list.py
@@ -0,0 +1,29 @@
+from .node import Node
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+      
+    def add_first(self, value):
+        new_node = Node(value)
+        new_node.next = self.head
+        self.head = new_node
+
+    def palindrome(self):
+      new_list = LinkedList()
+      current = self.head
+
+      while current:
+        new_list.add_first(current.val)
+        current = current.next
+
+      current = self.head
+      current_new_list = new_list.head
+      while current and current_new_list:
+        if current.val != current_new_list.val:
+          return False
+
+        current = current.next
+        current_new_list = current_new_list.next
+
+      return True
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #compare the two list and return another list with the indexes where differences are
 from algorithm_practice.arrays_algorithms import (is_palindrome,
       find_nth_element, pairs_with_given_sum, rotate_list_n2, merge_ordered_lists)
+from algorithm_practice.linked_lists.linked_list import LinkedList
 
 print(is_palindrome("kayak"))
 print(is_palindrome("racecar"))
@@ -29,3 +30,10 @@ list2 = ['0', '1','4','5','11','15']
 result = merge_ordered_lists(list1, list2)
 
 print(result)
+
+print("\n Testing palindrome in a Linked list: a, d, a nodes")
+test_list = LinkedList()
+test_list.add_first('a')
+test_list.add_first('d')
+test_list.add_first('a')
+print(test_list.palindrome())
