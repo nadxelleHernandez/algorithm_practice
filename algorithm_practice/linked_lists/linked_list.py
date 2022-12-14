cce0bf55081283f53671f1c1ee1dc8b5c13999c9
nadxelle <nadxellehernandez@gmail.com>
2022-12-14 14:45:16 -0800

    changed identation for linked list

--- algorithm_practice/linked_lists/linked_list.py
+++ algorithm_practice/linked_lists/linked_list.py
@@ -3,27 +3,33 @@ from .node import Node
 class LinkedList:
     def __init__(self):
         self.head = None
-      
+
     def add_top(self, value):
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
 
+    def get_first(self):
+        if not self.head:
+            return None
+            
+        return self.head.val
+
     def palindrome(self):
-      new_list = LinkedList()
-      current = self.head
+        new_list = LinkedList()
+        current = self.head
 
-      while current:
-        new_list.add_top(current.val)
-        current = current.next
+        while current:
+            new_list.add_top(current.val)
+            current = current.next
 
-      current = self.head
-      current_new_list = new_list.head
-      while current and current_new_list:
-        if current.val != current_new_list.val:
-          return False
+        current = self.head
+        current_new_list = new_list.head
+        while current and current_new_list:
+            if current.val != current_new_list.val:
+                return False
 
-        current = current.next
-        current_new_list = current_new_list.next
+            current = current.next
+            current_new_list = current_new_list.next
 
-      return True
+        return True
