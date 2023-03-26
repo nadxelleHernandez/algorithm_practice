ae88c4ad6bfced7579b96117df94ce148cf303d4
nadxelle <nadxellehernandez@gmail.com>
2023-03-26 16:29:51 -0700

    pascal triangle brute force implemeted and tested

--- algorithm_practice/divide_and_conquer.py
+++ algorithm_practice/divide_and_conquer.py
@@ -32,3 +32,48 @@ def smallest_missing_num(nums):
         return ini + 1
     else:
         return nums_len     
+    
+def pascal_triangle(n):
+    '''
+    Input: num_rows = 3
+    Output: [[1], [1, 1], [1, 2, 1]]
+    Explanation: The 0th index (first row) in the outer list contains one number: [1]. 
+    The 1st index (second row) contains two numbers: [1, 1]. 
+    The 2nd index (third row) can be calculated by setting the first and last indexes to 1 
+    and calculating the middle index using the two elements above it (1 + 1) 
+    so the third row is [1, 2, 1].
+
+    Input: num_rows = 3
+    Output: [[1], [1, 1], [1, 2, 1]]
+    Explanation: The 0th index (first row) in the outer list contains one number: [1]. 
+    The 1st index (second row) contains two numbers: [1, 1]. 
+    The 2nd index (third row) can be calculated by setting the first and last indexes to 1 
+    and calculating the middle index using the two elements above it (1 + 1) 
+    so the third row is [1, 2, 1].
+    '''
+
+    if n < 0:
+        return None
+    
+    if n == 0:
+        return []
+    
+    triangle = [[1]]
+    if n == 1:
+        return triangle
+    
+    triangle.append([1,1])
+    if triangle == 2:
+        return triangle
+    
+    for i in range(2,n):
+        triangle.append([1])
+        prev_row = triangle[i-1]
+        prev_row_len = len(prev_row) - 1 #could be reduced to half
+        for j in range(prev_row_len):
+            triangle[i].append(prev_row[j]+prev_row[j+1])
+
+        triangle[i].append(1)
+
+    return triangle
+
--- tests/test_divide_and_conquer.py
+++ tests/test_divide_and_conquer.py
@@ -1,4 +1,4 @@
-from algorithm_practice.divide_and_conquer import smallest_missing_num
+from algorithm_practice.divide_and_conquer import smallest_missing_num, pascal_triangle
 
 def test_smallest_missing_num_valid_array_with_missing_number():
     arr = [0, 1, 2, 6, 9, 11, 15]
@@ -34,3 +34,30 @@ def test_smallest_missing_num_valid_array_missing_last():
     result = smallest_missing_num(arr)
 
     assert result == 6
+
+def test_pascal_triangle_6():
+    n = 6
+
+    result = pascal_triangle(n)
+    
+    assert len(result) == 6
+    assert result == [
+    [1], 
+    [1, 1], 
+    [1, 2, 1], 
+    [1, 3, 3, 1], 
+    [1, 4, 6, 4, 1], 
+    [1, 5, 10, 10, 5, 1]
+]
+
+
+    assert result == 6
+
+def test_pascal_triangle_0():
+    n = 0
+
+    result = pascal_triangle(n)
+    
+    assert len(result) == 0
+    assert result == []
+
