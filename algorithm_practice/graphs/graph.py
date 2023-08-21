52352da08415e7486a1b41835bc8138bb7eb12b1
Nadxelle Hernandez <nadxelleHernandez@gmail.com>
2023-08-21 14:27:45 -0700

    Check if an adjacency graph is can be partitioned

--- algorithm_practice/graphs/graph.py
+++ algorithm_practice/graphs/graph.py
@@ -90,4 +90,127 @@ class Graph:
                 components_list.append(visited)
 
         return components_list
+    
+def create_color_dictionary(elements):
+    '''Creates a dictionary with the elements of the graph asigned to a non changed status
+    '''
+    if not elements:
+        return []
+    
+    colors = {}
+    for key in elements:
+        colors[key] = -1
 
+    return colors
+
+def possible_bipartition(graph):
+    if not graph:
+        return True
+    
+    queue = []
+    color_dict = create_color_dictionary(graph.keys())
+    elements = list(graph.keys())
+    current = elements[0]
+    color_dict[current] = 1
+    queue.append(current)
+
+    while queue or elements:
+        if not queue and elements:
+            current = elements[0]
+            queue.append(current)
+            color_dict[current] = 1
+        current = queue.pop(0)
+        for sibling in graph[current]:
+            if color_dict[sibling] == -1: #it hasn't been updated
+                queue.append(sibling)
+                if color_dict[current] == 1:
+                    color_dict[sibling] = 2
+                else:
+                    color_dict[sibling] = 1
+            elif color_dict[sibling] == color_dict[current]:
+                return False  
+        elements.remove(current)  
+    return True
+
+
+
+#def numIslands(grid: list[list[str]]) -> int:
+    '''Given an m x n 2D binary grid grid which represents a map of '1's (land) and '0's (water), 
+    return the number of islands.
+    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+        Example 1:
+
+        Input: grid = [
+        ["1","1","1","1","0"],
+        ["1","1","0","1","0"],
+        ["1","1","0","0","0"],
+        ["0","0","0","0","0"]
+        ]
+        Output: 1
+        Example 2:
+
+        Input: grid = [
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+        ]
+        Output: 3
+        
+
+        Constraints:
+
+        m == grid.length
+        n == grid[i].length
+        1 <= m, n <= 300
+        grid[i][j] is '0' or '1'.'''
+    # if not grid:
+    #     return 0
+
+    # num_islands = 0
+    # visited = []
+    # queue = [] 
+
+# def wordExistinTile(board: list[list[str]], word: str) -> bool:
+    '''https://leetcode.com/problems/word-search/
+        Each node would be one tile of the board
+        The edges would only go from tiles up, down, right or left.
+        To do a search we pick one element from the tile and check if it has been visited. 
+        If not we will add it to the queue and start the search
+        keep a current element from the word.
+        While the queue has elements:
+            if wordfromTiles are the same as the word return true
+            pop the tile, 
+            if is the same as the current element from the word 
+                add element to wordfromTiles
+                advance the current element of the word (if not possible break)
+                for each neighbor 
+                    if it is the same as the current char of the word
+                            add it to queue
+                            advance the current element of the word
+            else add to visited.
+        if the wordfromTiles are equal to the word return true.
+        else keep looking into the unvisited nodes.
+    '''
+    
+    # if not word or not board: 
+    #     return False
+    
+    # word_len = len(word)
+
+    # curr_len = 0
+    # queue = []
+    # rows = len(board)
+    # cols = len(board[0])
+    # visited = [[False for letter in range(cols)] for ls in range(rows)]
+    # current_letter = word[0]
+
+    # for i in range(rows):
+    #     for j in range(cols):
+    #         if not visited[i][j] and board[i][j] == current_letter:
+    #             curr_len += 1
+    #             # visitLeft(i,j)
+    #             # visitRight(i,j)
+    #             # visitUp(i,j)
+    #             # visitDown(i,j)
--- tests/test_graphs.py
+++ tests/test_graphs.py
@@ -55,3 +55,171 @@ def test_possible_bipartition_empty_input():
     # assert
     assert result == True
 
+def test_example_1():
+    # Arrange
+    dislikes = {
+      "Fido": [],
+      "Rufus": ["James", "Alfie"],
+      "James": ["Rufus", "T-Bone"],
+      "Alfie": ["Rufus"],
+      "T-Bone": ["James"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert answer
+
+def test_example_2():
+    dislikes = {
+      "Fido": [],
+      "Rufus": ["James", "Alfie"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Rufus", "James"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert not answer
+
+def test_example_3():
+    # Arrange
+    dislikes = {
+      "Fido": [],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["T-Bone", "James"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert not answer
+
+def test_will_return_true_for_a_graph_which_can_be_bipartitioned():
+    # Arrange
+    dislikes = {
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James"],
+      "T-Bone": ["Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert answer
+
+def test_will_return_false_for_graph_which_cannot_be_bipartitioned():
+    # Arrange
+    dislikes = {
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert not answer
+
+
+def test_will_return_true_for_empty_graph():
+    assert possible_bipartition({})
+  
+def test_will_return_false_for_another_graph_which_cannot_be_bipartitioned():
+    # Arrange
+    dislikes = {
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"],
+      "Calico": ["Nala"],
+      "Nala": ["Calico"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert not answer
+
+def test_multiple_dogs_at_beginning_dont_dislike_any_others():
+  # Arrange
+    dislikes = {
+      "Fido": [],
+      "Rufus": [],
+      "James": [],
+      "Alfie": ["T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["T-Bone"],
+      "Bruno": ["Nala"],
+      "Calico": ["Nala"],
+      "Nala": ["Bruno", "Calico"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert answer
+
+
+def test_multiple_dogs_in_middle_dont_dislike_any_others():
+    # Arrange
+    dislikes = {
+      "Fido": ["Alfie"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James"],
+      "T-Bone": [],
+      "Scruffy": ["Rufus"],
+      "Bruno": [],
+      "Spot": ["Nala"],
+      "Nala": ["Spot"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert answer
+
+def test_will_return_false_for_disconnected_graph_which_cannot_be_bipartitioned():
+    # Arrange
+    dislikes = {
+      "Katie": ["Michiko"],
+      "Michiko": ["Katie"],
+      "Fido": ["Alfie", "Bruno"],
+      "Rufus": ["James", "Scruffy"],
+      "James": ["Rufus", "Alfie"],
+      "Alfie": ["Fido", "James", "T-Bone"],
+      "T-Bone": ["Alfie", "Scruffy"],
+      "Scruffy": ["Rufus", "T-Bone"],
+      "Bruno": ["Fido"]
+    }
+
+    # Act
+    answer = possible_bipartition(dislikes)
+
+    # Assert
+    assert not answer
+
