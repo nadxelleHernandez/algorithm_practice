74d3c824b1ad507dec2c570b52e8a8c230c767f7
Nadxelle Hernandez <nadxelleHernandez@gmail.com>
2023-10-24 13:36:00 -0700

    Added mars exploration algorithm

--- algorithm_practice/string_algorithms.py
+++ algorithm_practice/string_algorithms.py
@@ -8,3 +8,33 @@ def camelcase(s)-> int:
             words += 1
 
     return words
+
+def marsExploration(s):
+    '''
+    s = SOSSPSSQSSOR
+
+    Expected signal: SOSSOSSOSSOS
+    Recieved signal: SOSSPSSQSSOR
+    Difference:          X  X   X
+    
+    returns 3
+    '''
+
+    if not s:
+        raise ValueError("The string of data is Empty")
+    
+    s_len = len(s)
+    if s_len % 3 != 0:
+        raise ValueError("The string of data is incomplete")
+    
+    help_str = "SOS"
+    help_i = 0
+    char_changed = 0
+    for char in s:
+        if help_i == 3:
+            help_i = 0
+        if char != help_str[help_i]:
+            char_changed += 1
+        help_i += 1
+
+    return char_changed
--- tests/test_string_algorithms.py
+++ tests/test_string_algorithms.py
@@ -1,4 +1,5 @@
-from algorithm_practice.string_algorithms import camelcase
+from algorithm_practice.string_algorithms import camelcase, marsExploration
+import pytest
 
 def test_camelcase_camelcase_string():
     s = "camelCaseString"
@@ -6,3 +7,26 @@ def test_camelcase_camelcase_string():
     result = camelcase(s)
 
     assert result == 3
+
+def test_marsExploration_altered_SOS():
+    s = "SOSSPSSQSSOR"
+
+    result = marsExploration(s)
+
+    assert result == 3
+
+def test_marsExploration_non_altered_SOS():
+    s = "SOSSOSSOSSOS"
+
+    result = marsExploration(s)
+
+    assert result == 0
+
+
+def test_marsExploration_empty_SOS():
+    with pytest.raises(ValueError):
+        marsExploration("")
+
+def test_marsExploration_incomplete_SOS():
+    with pytest.raises(ValueError):
+        marsExploration("SOSSPSSQSSO")
