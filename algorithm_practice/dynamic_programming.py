e33470f5f907ab92db4ab4ea25b98e65166ec369
Nadxelle Hernandez <nadxelleHernandez@gmail.com>
2023-11-02 15:53:56 -0700

    Added max subsequence with k sustitutions

--- algorithm_practice/dynamic_programming.py
+++ algorithm_practice/dynamic_programming.py
@@ -115,27 +115,53 @@ def max_contiguous_sum(arr):
 
     return max_sum
 
-    # while j < length:
-    #     sum += arr[j]
-    #     if arr[i] >= max_sum or sum > max_sum:
-    #         max_sum = max(arr[i],sum)
-            
-    #     else:
-    #         i += 1
-    #         sum = arr[i]
-    #         j = i
+def get_max_subarray_with_k_substitutions(s:str, k:int)->int:
+    '''
+    You are given a string s and an integer k. You can choose any character of the string and change it to any other uppercase English character. 
+    You can perform this operation at most k times.
+    Return the length of the longest substring containing the same letter you can get after performing the above operations.
 
-    #     j += 1
-        # sum = arr[i] + arr[j]
-        # if arr[i] > max_sum or sum > max_sum: #The sum is increasing
-        #     max_sum = max(arr[i],sum)
-        #     actual_sum += sum
-        # else:
-        #     actual_sum = 0
-        # i += 1 
-        # j += 1
+    Input: s = "ABAB", k = 2
+    Output: 4
+    Explanation: Replace the two 'A's with two 'B's or vice versa.
 
-    return max_sum
+    Input: s = "AABABBA", k = 1
+    Output: 4
+    '''
+
+    if not s:
+        return 0
+    s_len = len(s)
+    if s_len == 1:
+        return 1
+    
+    frequency_dict = {}
+    longest = 1
+    max_freq = 0
+    left = 0
+    right = 0
+
+    while right<s_len:
+        current_char = s[right]
+        if current_char not in frequency_dict:
+            frequency_dict[current_char] = 1
+        else:
+            frequency_dict[current_char] += 1
+
+        max_freq = max(max_freq, frequency_dict[current_char])
+        window_size = right - left + 1
+        if (window_size - max_freq) > k :  #I can't make the substring bigger, therefore I have the max_substring with this character
+            frequency_dict[s[left]] -= 1 #we assumed we could use it but now we are moving the window and maybe is another character
+            left += 1
+
+        longest = max(longest, right - left+1)
+        right += 1
+
+    return longest
+
+
+    
+
 
 def get_max_sum_subarray_k(arr, k):
     '''
